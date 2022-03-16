187f4a28e7f949bb29e6f970eca5f75ec49cfb25
himharsh1997 <himharsh1997@gmail.com>
2022-03-16 12:21:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-facebook-login-example-d2tigc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-facebook-login-example-d2tigc)
---
+++ env.js
@@ -0,0 +1,5 @@
+// moved env variables into js file for stackblitz because it doesn't support dotenv (.env)
+export default {
+    REACT_APP_API_URL: 'http://localhost:4000',
+    REACT_APP_FACEBOOK_APP_ID: '314930319788683'
+};
